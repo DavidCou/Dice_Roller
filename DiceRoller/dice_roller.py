2d2fc14b38a02ecee1046cd54ca7b249ae42413f
David Courtney <davidcourtney606@gmail.com>
2025-02-05 12:50:08 -0500

    Minor bug fixes

--- DiceRoller/dice_roller.py
+++ DiceRoller/dice_roller.py
@@ -11,14 +11,10 @@ class DiceRoller:
             num_sides = self.get_user_input("How many sides should each die have? (Enter a whole number greater than 1): ", 2)
 
             self.create_dice(num_dice, num_sides)
-            input(f"Press enter to roll your {num_dice} D{num_sides}: ")
-            self.roll_dice()
-
-            roll_again = input("Would you like to roll more dice? (y/n): ").lower()
-            if roll_again == "n":
-                print("Thanks for playing!")
-                break
-
+            input(f"Press any key to roll your {num_dice} D{num_sides}: ")
+            self.roll_dice(num_dice)
+            self.ask_to_roll_again()
+            
     def get_user_input(self, prompt, min_value):
         """Prompt the user for input and validate it as an integer greater than or equal to min_value."""
         while True:
@@ -28,26 +24,34 @@ class DiceRoller:
                     raise ValueError(f"Value must be a whole number greater than or equal to {min_value}.")
                 return value
             except ValueError as e:
-                print(f"Invalid input! {e}")
+                print(f"Invalid input! Please try again with a valid number.")
 
     def create_dice(self, num_dice, num_sides):
         """Create a list of Die objects based on user input."""
-        from .die import Die  # Import Die class here to avoid circular imports
+        from .die import Die  # Import Die class here instead of at the top of the file to avoid circular imports in __init__.py
         self.dice = [Die(num_sides) for _ in range(num_dice)]
 
-    def roll_dice(self):
+    def roll_dice(self, num_dice):
         """Roll all dice and display the results."""
-        roll_total = 0
-        for i, die in enumerate(self.dice, 1):
-            current_roll = die.roll_die()
-            roll_total += current_roll
-            print(f"Die {i} rolled: {current_roll}")
-        print(f"Total roll: {roll_total}")
-
-
-
-
-                # # If only one die is to be rolled
-                # die = Die(num_sides)
-                # roll_die = input(f"Press enter to roll your {num_sides} sided die: ")
-                # print(f"Your die rolled: {die.roll_die()}")
+        if num_dice > 1:
+            roll_total = 0
+            for i, die in enumerate(self.dice, 1):
+                current_roll = die.roll_die()
+                roll_total += current_roll
+                print(f"Die {i} rolled: {current_roll}")
+            print(f"Your dice rolled for a total of {roll_total}")
+        else:
+            roll = self.dice[0].roll_die()
+            print(f"Your die rolled a {roll}")
+    
+    def ask_to_roll_again(self):
+        """Ask the user if they want to roll again"""
+        while True:
+            roll_again = input("Would you like to roll more dice? (y/n): ").lower()
+            if roll_again == "y":
+                break 
+            elif roll_again == "n":
+                print("Goodbye!")
+                exit()
+            else:
+                print("Invalid input! Please enter 'y' to roll again or 'n' to exit.")
